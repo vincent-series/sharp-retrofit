1fa2ab9b4e0b4dada0cbc5a8c4d7cfadda5d0b3b
喜欢、陪你看风景 <programmer_zzq@163.com>
2017-09-23 11:18:09 +0800

    update

--- app/src/main/java/zzq/code/utils/Utils.java
+++ app/src/main/java/zzq/code/utils/Utils.java
@@ -1,7 +1,12 @@
 package zzq.code.utils;
 
 import android.app.Application;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.ColorInt;
+import android.support.annotation.ColorRes;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.util.TypedValue;
 
 /**
@@ -16,19 +21,33 @@ public class Utils {
         sApplication = NullUtil.checkNonNull(application,"初始化Utils的Application对象不可为Null!");
     }
 
+    private static Application getApplication(){
+        return NullUtil.checkNonNull(sApplication,"尚未使用Application初始化Utils");
+    }
+
     public static class DimenUtil{
 
         public static float dp2px(float dp){
-            return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,dp,sApplication.getResources().getDisplayMetrics());
+            return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,dp,getApplication().getResources().getDisplayMetrics());
         }
 
         public static float sp2px(float sp){
-            return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,sp,sApplication.getResources().getDisplayMetrics());
+            return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,sp,getApplication().getResources().getDisplayMetrics());
         }
-
-
     }
 
+
+    public static class ResUtil{
+
+        public static @ColorInt int getColor(@ColorRes int colorRes){
+            return ContextCompat.getColor(getApplication(),colorRes);
+        }
+
+        public static Drawable getDrawable(@DrawableRes int drawableRes){
+            return ContextCompat.getDrawable(getApplication(),drawableRes);
+        }
+
+    }
 
 
 
