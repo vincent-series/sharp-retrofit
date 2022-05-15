3d86c465821f702e5da988fc75586b420d2a5283
vincent.zhu <coder_zzq@aliyun.com>
2022-05-15 10:30:21 +0800

    setup call timeout.

--- demo/src/main/java/com/example/sharp_retrofit/ApiServiceManager.kt
+++ demo/src/main/java/com/example/sharp_retrofit/ApiServiceManager.kt
@@ -3,6 +3,7 @@ package com.example.sharp_retrofit
 import com.coder.vincent.sharp_retrofit.bean.BaseUrlPlaceholder
 import com.coder.vincent.sharp_retrofit.call_adapter.flow.FlowCallAdapterFactory
 import com.coder.vincent.sharp_retrofit.interceptors.BaseUrlInterceptor
+import com.coder.vincent.sharp_retrofit.interceptors.TimeoutInterceptor
 import com.example.sharp_retrofit.api_service.WeatherApiService
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
@@ -17,9 +18,11 @@ object ApiServiceManager {
         .client(
             OkHttpClient.Builder()
                 .addInterceptor(BaseUrlInterceptor())
+                .addInterceptor(TimeoutInterceptor())
                 .connectTimeout(10, TimeUnit.SECONDS)
                 .writeTimeout(10, TimeUnit.SECONDS)
                 .readTimeout(10, TimeUnit.SECONDS)
+                .callTimeout(30, TimeUnit.SECONDS)
                 .build()
         )
         .build()
