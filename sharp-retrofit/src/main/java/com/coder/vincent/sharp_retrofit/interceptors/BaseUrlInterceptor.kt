e7682bd0608ba1a1b144a31d80a772fe3014bcd1
vincent.zhu <coder_zzq@aliyun.com>
2022-05-20 20:20:13 +0800

    validate base url. cache only base url instead of base url and encoded paths.

--- sharp-retrofit/src/main/java/com/coder/vincent/sharp_retrofit/interceptors/BaseUrlInterceptor.kt
+++ sharp-retrofit/src/main/java/com/coder/vincent/sharp_retrofit/interceptors/BaseUrlInterceptor.kt
@@ -14,15 +14,16 @@ class BaseUrlInterceptor : Interceptor {
         val srcRequest = chain.request()
         val srcUrl = srcRequest.url()
         srcRequest.tag(Invocation::class.java)?.method()?.let { method ->
-            val newUrl = parseOverridingUrl(method, srcUrl)
+            val newUrl = parseOverridingUrl(method)
             if (newUrl !== NO_OVERRIDING) {
                 return@intercept chain.proceed(
                     srcRequest.newBuilder()
                         .url(
-                            newUrl.newBuilder()
-                                .encodedUsername(srcUrl.encodedUsername())
-                                .encodedPassword(srcUrl.encodedPassword())
-                                .encodedQuery(srcUrl.encodedQuery())
+                            srcUrl.newBuilder()
+                                .scheme(newUrl.scheme())
+                                .host(newUrl.host())
+                                .port(newUrl.port())
+                                .encodedPath(newUrl.encodedPath() + srcUrl.jointEncodedPath())
                                 .build()
                         )
                         .build()
@@ -35,19 +36,18 @@ class BaseUrlInterceptor : Interceptor {
 
 private val cachedOverridingUrls = mutableMapOf<Method, HttpUrl>()
 private val NO_OVERRIDING = BaseUrlPlaceholder.instance
-private fun parseOverridingUrl(invokeMethod: Method, srcUrl: HttpUrl): HttpUrl {
+private fun parseOverridingUrl(invokeMethod: Method): HttpUrl {
     cachedOverridingUrls[invokeMethod]?.let { return@parseOverridingUrl it }
     synchronized(BaseUrlInterceptor::class) {
         cachedOverridingUrls[invokeMethod]?.let { return@parseOverridingUrl it }
         val baseUrlStr = invokeMethod.getAnnotation(BaseUrl::class.java)?.value
             ?: invokeMethod.declaringClass.getAnnotation(BaseUrl::class.java)?.value
         baseUrlStr?.let {
-            val overridingUrl =
-                HttpUrl.get(it).newBuilder()
-                    .addEncodedPathSegments(srcUrl.jointEncodedPath())
-                    .build()
-            cachedOverridingUrls[invokeMethod] = overridingUrl
-            return@parseOverridingUrl overridingUrl
+            if (!it.endsWith("/")) throw IllegalStateException("base url must be ended with /. please fix the api interface #${invokeMethod.declaringClass.name}.${invokeMethod.name}#'s base url:$it")
+            HttpUrl.get(it).let { httpUrl ->
+                cachedOverridingUrls[invokeMethod] = httpUrl
+                return@parseOverridingUrl httpUrl
+            }
         }
         cachedOverridingUrls[invokeMethod] = NO_OVERRIDING
         return@parseOverridingUrl NO_OVERRIDING
